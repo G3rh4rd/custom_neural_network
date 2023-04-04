eb3ec7eda32cda2267e09cc55c2839252b926f4e
GeDi <einkauf@heero-sports.de>
2023-04-04 10:14:08 +0200

    added cmake

--- c/include/activation_functions.h
+++ c/include/activation_functions.h
@@ -2,7 +2,7 @@
 #define ACTIVATION_FUNCTIONS_H
 
 typedef float activation_function_t(float weights_sum);
-
+/*
 typedef activation_function_t binary_step_function;
 typedef activation_function_t linear_function;
 typedef activation_function_t sigmoid_function;
@@ -10,5 +10,5 @@ typedef activation_function_t tanh_function;
 typedef activation_function_t relu_function;
 typedef activation_function_t relu_leaky_function;
 typedef activation_function_t softmax_function;
-
+*/
 #endif /* ACTIVATION_FUNCTIONS_H */
---
+++ c/src/main.c
@@ -0,0 +1,6 @@
+#include "perceptron.h"
+#include "activation_functions.h"
+
+int main() {
+    
+}
